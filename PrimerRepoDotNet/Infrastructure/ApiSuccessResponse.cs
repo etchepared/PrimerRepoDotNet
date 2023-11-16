9ec04355efaa3ad91d566067403f9a671da72e75
etchepared <etcheparede@gmail.com>
2023-11-16 00:58:08 -0300

    feat add ResponseFactory

---
+++ PrimerRepoDotNet/Infrastructure/ApiSuccessResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices.ObjectiveC;
+
+namespace PrimerRepoDotNet.Infrastructure
+{
+    public class ApiSuccessResponse
+    {
+        public int Status { get; set; }
+        public object? Data { get; set; }
+    }
+}
